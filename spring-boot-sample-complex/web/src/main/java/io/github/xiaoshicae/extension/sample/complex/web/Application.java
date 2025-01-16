46eb2d8b6be200fcabe07702130a4e4f7083304b
xiaoshicae <snk_zhuang@126.com>
2025-01-16 10:34:06 +0800

    @feature:升级依赖

--- spring-boot-sample-complex/web/src/main/java/io/github/xiaoshicae/extension/sample/complex/web/Application.java
+++ spring-boot-sample-complex/web/src/main/java/io/github/xiaoshicae/extension/sample/complex/web/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.github.xiaoshicae.extension.spring.boot.autoconfigure.annotation.ExtensionScan;
 
+
+/**
+ * Admin: http://127.0.0.1:8080/my-extension-admin
+ */
 @SpringBootApplication
 @ExtensionScan(scanPackages = "io.github.xiaoshicae.extension.sample.complex")
 public class Application {
